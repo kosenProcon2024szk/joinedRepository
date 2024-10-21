7bdd16f4b577898fd8a01af57309c2eb654c7bef
r04i02 <k10190202@gmail.com>
2024-10-21 11:20:45 +0900

    initialize repository

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const tytleName: string = '高専プロコン2024競技部門'
+  return (
+    <div className="App">
+      <h1>{tytleName}</h1>
+    </div>
+  );
+}
+
+export default App;
